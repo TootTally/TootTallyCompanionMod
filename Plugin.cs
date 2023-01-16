8a55d208ee8f35387576d441c7a1153e422c698c
Electrostats <Super_justy@hotmail.com>
2023-01-16 13:17:15 -0500

    Version bump for release

--- Plugin.cs
+++ Plugin.cs
@@ -36,7 +36,7 @@ namespace TootTally
 
         public static Plugin Instance;
         public static SerializableClass.User userInfo; //Temporary public
-        public const int BUILDDATE = 20230116;
+        public const int BUILDDATE = 20230117;
         public ConfigEntry<string> APIKey { get; private set; }
         public ConfigEntry<bool> AllowTMBUploads { get; private set; }
         public ConfigEntry<bool> ShouldDisplayToasts { get; private set; }
