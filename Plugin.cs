db21befd0f45e6592be5b1e01ddf62eac5414110
Electrostats <Super_justy@hotmail.com>
2023-08-24 00:23:17 -0400

    Build Bump

--- Plugin.cs
+++ Plugin.cs
@@ -37,7 +37,7 @@ namespace TootTally
         public const string PLUGIN_FOLDER_NAME = "TootTally-TootTally";
         public static Plugin Instance;
         public static SerializableClass.User userInfo; //Temporary public
-        public const int BUILDDATE = 20230823;
+        public const int BUILDDATE = 20230824;
         internal ConfigEntry<string> APIKey { get; private set; }
         public ConfigEntry<bool> ShouldDisplayToasts { get; private set; }
 
